bddd99f42b44716f5fe60cc10a92971315d42596
theonlyrehman <166296111+theonlyrehman@users.noreply.github.com>
2024-05-15 05:48:35 +0500

    this is cli based currency converter

---
+++ index.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+
+const currency:any = {
+    USD: 1, //base currency
+    EUR: 0.92,
+    GBP: 0.79,
+    INR: 83.49,
+    PKR: 278
+};
+
+let answer = await inquirer.prompt (
+    [
+        {
+            name: "from",
+            message: "Enter from currency",
+            type: "list",
+            choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name: "to",
+            message: "Enter to currency",
+            type: "list",
+            choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name: "amount",
+            message:"Enter you amount",
+            type: "number"
+        }
+    ]
+)
+
+let fromAmount = currency[answer.from]
+
+let toAmount = currency[answer.to]
+
+let amount = answer.amount
+
+let baseAmount = amount / fromAmount // USD base amount
+
+let convertedAmount = baseAmount * toAmount
+
+console.log(convertedAmount);
